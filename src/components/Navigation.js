a8264475b4e2d97d96e6a798fbf7cc8ef5856dfd
Samuel Garcia <sam.garcia@dal.ca>
2025-09-25 11:17:02 -0300

    Initial Navigation and page setup.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,27 @@
-import react from "react";
 import "../css/NavBar.css"
-
+import { VscMenu }  from "react-icons/vsc"
+import { useState } from "react"
 
 function NavBar () {
 
+    const [menuDown, setMenuDown] = useState(false) 
+
+    const handleMenuClick = () => {
+        setMenuDown(!menuDown)
+    }
+
     return (
-        <div className="navbar">
+        <div className={`navbar ${menuDown ? "open" : ""}`}>
+            <h1>SG</h1>
             <ul>
-                <h1>Samuel Garcia</h1>
                 <li>About Me</li>
                 <li>Experience</li>
                 <li>Projects</li>
                 <li>Contact</li>
             </ul>
+            <button className="navbar-hamburger" onClick={handleMenuClick}>
+                <VscMenu className="menu"/>
+            </button>
         </div>
     )
 
