753c9fdfe1e9693b15d359a893f77040860d8eab
Samuel Garcia <sam.garcia@dal.ca>
2025-09-25 09:59:53 -0300

    Initial Push

---
+++ src/components/Navigation.js
@@ -0,0 +1,21 @@
+import react from "react";
+import "../css/NavBar.css"
+
+
+function NavBar () {
+
+    return (
+        <div className="navbar">
+            <ul>
+                <h1>Samuel Garcia</h1>
+                <li>About Me</li>
+                <li>Experience</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+
+}
+
+export default NavBar
