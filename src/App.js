3cdb7d1170a69de08a0e3c96e6128ae0f167088a
Samuel Garcia <sam.garcia@dal.ca>
2025-11-15 07:44:26 -0400

    Added missing imports.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Pages
 import Home from './pages/Home';
 import Resume from './pages/Resume';
+import Experience from './pages/Experience';
+import Projects from './pages/Projects';
 
 // Components
 import NavBar from './components/Navigation';
